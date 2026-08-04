6829c0236e7650f36b36529db391dce29bfdb22f
PedroEustaq <pedroeustaquioassad@gmail.com>
2026-08-04 20:25:31 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,3 +1,11 @@
 <?php
 
-echo password_hash("admin", PASSWORD_DEFAULT);
+session_start();
+
+$_SESSION["teste"] = "OK";
+
+echo "Session ID: " . session_id() . "<br>";
+
+echo "<pre>";
+print_r(headers_list());
+echo "</pre>";
