b930d2798f6db9b4c2174e6a581ee3ae6ba4f342
PedroEustaq <pedroeustaquioassad@gmail.com>
2026-08-05 09:39:22 -0300

    AtualizandoNav

--- rodape.php
+++ rodape.php
@@ -2,7 +2,7 @@
 
 
 echo "<footer>";
-echo "<p>Desenvolvido por <a href='https://pedroeustaquio.vercel.app' target='_blank'>Pedro Eustáquio</a></p>";
+echo "<p>Desenvolvido por <a class='nav-link' href='https://pedroeustaquio.vercel.app' target='_blank'>Pedro Eustáquio</a></p>";
 echo "</footer>";
 
 ?>
